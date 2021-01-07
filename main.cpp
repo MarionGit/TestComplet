c3f230cc743ccb2da3e4c9aefcdc1ac29865db34
GitUser <GitUser@email.com>
2021-01-07 09:55:38 +0100

    branche 2

--- main.cpp
+++ main.cpp
@@ -6,5 +6,6 @@ int main()
     std::cout << "Hello Git!" << std::endl;
     std::cout << "C'est moi!" << std::endl;
     std::cout << "Voila!" << std::endl;
+    std::cout << "branche 2" << std::endl;
     return 0;
 }
